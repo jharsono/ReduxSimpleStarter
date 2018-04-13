edfb92d443b514c4d7c8f1a826cee34cf2e0eb05
Joshua Harsono <jharsono@gmail.com>
2018-04-13 16:50:43 -0700

    add search on termchange with debounce 300ms

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -18,12 +18,17 @@ class SearchBar extends Component { // define a new class called SearchBar and i
 
   render() {
     return(
-        <div>
+        <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.setState( {term: event.target.value})} />
+          onChange={event => this.onInputChange(event.target.value)} />
         </div>
       ); //input field is a controlled input. when the state changes, the input field changes.
+    }
+    
+    onInputChange(term) {
+      this.setState({term});
+      this.props.onSearchTermChange(term);
     }
 }
 export default SearchBar;
